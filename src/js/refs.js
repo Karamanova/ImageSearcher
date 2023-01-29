beacdccfe4ceb4605246ff648a3204dd3812852a
Maryna Karamanova <karamanova91@gmail.com>
2023-01-29 14:36:23 +0200

    scroll fixed

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  form: document.querySelector('.search-form'),
+  submitBtn: document.querySelector('.search-form__button'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
